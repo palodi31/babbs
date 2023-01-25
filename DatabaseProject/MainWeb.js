d6dd3c5e54d860a29db1deb1909f82b05f6efc36
palodi31 <123529195+palodi31@users.noreply.github.com>
2023-01-25 12:06:40 +0800

    Delete DatabaseProject directory

--- DatabaseProject/MainWeb.js
+++
@@ -1,7 +0,0 @@
- // JavaScript code goes here
- const addToCartButtons = document.querySelectorAll('.add-to-cart-button');
- addToCartButtons.forEach(button => {
-     button.addEventListener('click', () => {
-         alert('Product added to cart!');
-     });
- });
